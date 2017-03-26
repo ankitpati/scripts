cad634bc1f1e36b6ccbe9e9c1348cd62691dc8c3
Ankit Pati <ankitpati@gmail.com>
2017-03-26 22:23:01 +0530

    simplify logic by removing unnecessary floating-point math

--- src/perror.c
+++ src/perror.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <errno.h>
 
 int main(int argc, char **argv)
 {
     int i;
-    char *n;
-
-    /* (1 + floor(log(~0u))) == maximum digits in int,
-     * + 1 for '\0'
-     */
-    if(!(n = malloc( sizeof(char) * (1 + floor(log(~0u)) + 1) )))
-        return 12; /* perror(12) == cannot allocate memory */
 
     if (argc < 2) {
         fprintf(stderr, "Usage:\n\tperror <errno>...\n");
@@ -20,8 +12,8 @@ int main(int argc, char **argv)
     }
 
     for (i = 1; i < argc; ++i) {
-        sprintf(n, "%d", errno = atoi(argv[i]));
-        perror(n);
+        fprintf(stderr, "%d: ", errno = atoi(argv[i]));
+        perror("");
     }
 
     return 0;
