a8eff3feb2e2e2bf2372d8f07f6f591cc4ee2f65
Ankit Pati <ankitpati@gmail.com>
2017-04-25 07:04:49 +0530

    run Perl tests adhering to prove convention

---
+++ src/pt.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+die "Usage:\n\tbt <filename>... [--no-select]\n"
+    if !@ARGV || (grep (/^--no-select$/, @ARGV) && @ARGV <  2);
+
+my $noselect = 1 if grep /^--no-select$/, @ARGV;
+@ARGV = grep !/^--no-select$/, @ARGV;
+
+$ENV{HARNESS_PERL_SWITCHES} = '-MDevel::Cover';
+
+s|^(.*/)?(.*)\.pm$|${\($1 // '')}t/$2.t| foreach @ARGV;
+
+system qw(find . -type d -name cover_db -exec rm -rf {} +);
+system qw(prove -v), @ARGV;
+
+s|^t/||, s|/t/|/|, s|\.t$|\.pm| foreach @ARGV;
+system 'cover', ( $noselect ? qw(-ignore_re .*\.t$)
+            : ('-select_re', '^'.( join '|', map(quotemeta, @ARGV) ).'$') );
