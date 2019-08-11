84b1542a75693c6f9498b32842388ccadb7c94da
Ankit Pati <contact@ankitpati.in>
2019-08-12 02:08:50 +0530

    Perl script to extract failures from test logs

---
+++ src/not-ok.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $me = (split m|/|, $0)[-1];
+
+my ($src, $dest) = (shift, shift);
+die "Usage:\n\t$me [source-file] [destination-file]\n" if @ARGV;
+
+my ($fin, $fout);
+open $fin, '<', $src if $src;
+open $fout, '>', $dest if $dest;
+$fin //= \*STDIN;
+$fout //= \*STDOUT;
+
+while (<$fin>) {
+    next if /^ok \d+ [-#] / .. /^}$/
+         or /^\s+ok \d+ [-#] /
+         or /: [\d.]+ wallclock secs \(/
+         or /^\s*}$/
+         or /^\s*\d+\.\.\d+$/
+    ;
+    s/ \{$// if /^\s*not ok \d+ [-#] /;
+    print $fout $_;
+}
