6192880411fa4dc655ab5c2b6b675a2d2b48200f
Ankit Pati <contact@ankitpati.in>
2019-08-12 01:18:20 +0530

    case-insensitive file search

--- src/bf.pl
+++ src/bf.pl
@@ -7,16 +7,18 @@ use warnings;
 
 use Cwd;
 
-@ARGV or die "Usage:\n\tbf <filname glob>...\n";
+my $me = (split m|/|, $0)[-1];
 
-my ($username) = cwd =~ qr{^/home/(.*?/|.*)};
-$username or die "Unsupported working directory!\n";
-$username =~ s|/||;
+@ARGV or die "Usage:\n\t$me <filname glob>...\n";
 
 my @name;
-push @name, (m|/| ? '-path' : '-name', "*$_*", '-o') foreach @ARGV;
+push @name, (m|/| ? '-ipath' : '-iname', "*$_*", '-o') foreach @ARGV;
 pop (@name), unshift (@name, '('), push (@name, ')') if @name;
 
-chdir "/home/$username";
-system qw(find . -not ( -path */cover_db -prune -or -path */.git -prune )
-                 ( -type f -or -type d )), @name;
+exec qw[find . -not (
+                      -path */cover_db -prune -or
+                      -path */.git     -prune -or
+                      -path */tmp      -prune -or
+                      -path */web/run  -prune
+                    )
+               ( -type f -or -type d )], @name;
