20809e97dac534b97a9590980f527271509d24f6
Ankit Pati <contact@ankitpati.in>
2019-08-22 21:30:49 +0530

    washwhale, now washes whales better!

--- src/washwhale.sh
+++ src/washwhale.sh
@@ -7,7 +7,7 @@ vols="$(docker volume ls -q)"
 test -n "$vols" && docker volume rm $vols
 
 dang="$(docker images -a --filter=dangling=true -q)"
-test -n "$dang" && docker rmi "$dang"
+test -n "$dang" && docker rmi $dang
 
 imgs="$(docker images -a -q)"
 #test -n "$imgs" && docker rmi --force $imgs
