2d24f824e937eb974f7365bc108431d779788d32
Ankit Pati <contact@ankitpati.in>
2019-08-29 10:41:02 +0530

    support for the force-push in git publish

--- src/git-publish.sh
+++ src/git-publish.sh
@@ -2,15 +2,17 @@
 
 myname="$(basename "$0")"
 
-test "$#" -eq 0 && echo "Usage: $myname <branch>..." && exit 1
+test '-f' = "$1" && shift && force=('-f')
+
+test "$#" -eq 0 && echo "Usage: $myname [-f] <branch>..." && exit 1
 test -z "$(git rev-parse --show-toplevel)" && \
     echo "$myname: present directory must be in a Git repository" && exit 2
 
 for branch in "$@"
 do
-    git push sgh "$branch" &
-    git push sgl "$branch" &
-    git push sbb "$branch" &
+    git push "${force[@]}" sgh "$branch" &
+    git push "${force[@]}" sgl "$branch" &
+    git push "${force[@]}" sbb "$branch" &
 done
 
 wait # guard against premature prompt
