d5292f5e531bcb87f3ee3ebb3ad178fa5a087d75
Ankit Pati <contact@ankitpati.in>
2019-08-29 11:00:36 +0530

    support all git-push options

--- src/git-publish.sh
+++ src/git-publish.sh
@@ -2,17 +2,29 @@
 
 myname="$(basename "$0")"
 
-test '-f' = "$1" && shift && force=('-f')
+args=("$@")
 
-test "$#" -eq 0 && echo "Usage: $myname [-f] <branch>..." && exit 1
+for i in "${!args[@]}"
+do
+    if [[ "${args[$i]}" == -* ]]
+    then
+        pushparams=("${pushparams[@]}" "${args[$i]}")
+        unset 'args[$i]'
+    fi
+done
+
+test "${#args[@]}" -eq 0 \
+    && echo "Usage: $myname [-<git-push-params>...] <branch>..." \
+    && exit 1
+
 test -z "$(git rev-parse --show-toplevel)" && \
     echo "$myname: present directory must be in a Git repository" && exit 2
 
-for branch in "$@"
+for branch in "${args[@]}"
 do
-    git push "${force[@]}" sgh "$branch" &
-    git push "${force[@]}" sgl "$branch" &
-    git push "${force[@]}" sbb "$branch" &
+    git push "${pushparams[@]}" sgh "$branch" &
+    git push "${pushparams[@]}" sgl "$branch" &
+    git push "${pushparams[@]}" sbb "$branch" &
 done
 
 wait # guard against premature prompt
