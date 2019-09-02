4112850df8f8ae59af33320e6a503b710893fee6
Ankit Pati <contact@ankitpati.in>
2019-09-02 10:18:56 +0530

    remove more redundancy, generate HTTPS & SSH urls from common base

--- src/git-prep2pub.sh
+++ src/git-prep2pub.sh
@@ -15,20 +15,20 @@ aws_region="${3:-ap-south-1}"
 
 declare -A map
 map=(
-    [gh]="https://$username@github.com/$username"
-    [gl]="https://$username@gitlab.com/$username"
-    [bb]="https://$username@bitbucket.org/$username"
-    [az]="https://$username@git-codecommit.$aws_region.amazonaws.com/v1/repos"
-    [nb]="https://$username@notabug.org/$username"
-    [sgh]="ssh://github.com/$username"
-    [sgl]="ssh://gitlab.com/$username"
-    [sbb]="ssh://bitbucket.org/$username"
-    [saz]="ssh://git-codecommit.$aws_region.amazonaws.com/v1/repos"
-    [snb]="ssh://notabug.org/$username"
+    [az]="git-codecommit.$aws_region.amazonaws.com/v1/repos"
+    [bb]="bitbucket.org/$username"
+    [gh]="github.com/$username"
+    [gl]="gitlab.com/$username"
+    [nb]="notabug.org/$username"
 );
 
 for remote in "${!map[@]}"
 do
+    hostpath="${map[$remote]}/$repository.git"
+
     git remote remove "$remote"
-    git remote add "$remote" "${map[$remote]}/$repository.git"
+    git remote add "$remote" "https://$username@$hostpath"
+
+    git remote remove "s$remote"
+    git remote add "s$remote" "ssh://$hostpath"
 done
