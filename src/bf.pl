039296c4581b8a3e7ac9ce627c24b3887178c120
Ankit Pati <contact@ankitpati.in>
2019-09-13 08:18:54 +0530

    more modular find & search tools

--- src/bf.pl
+++ src/bf.pl
@@ -7,18 +7,24 @@ use warnings;
 
 use Cwd;
 
+# User-Serviceable Parts
+my @ignore_directories = qw(.git __pycache__ cover_db);
+my @find_types = qw(d f);
+# End of User-Serviceable Parts
+
 my $me = (split m|/|, $0)[-1];
 
 @ARGV or die "Usage:\n\t$me <filname glob>...\n";
 
-my @name;
+my (@name, @path, @type);
+
 push @name, (m|/| ? '-ipath' : '-iname', "*$_*", '-o') foreach @ARGV;
 pop (@name), unshift (@name, '('), push (@name, ')') if @name;
 
-exec qw[find . -not (
-                      -path */cover_db -prune -or
-                      -path */.git     -prune -or
-                      -path */tmp      -prune -or
-                      -path */web/run  -prune
-                    )
-               ( -type f -or -type d )], @name;
+push @path, '-path', "*/$_", qw(-prune -or) foreach @ignore_directories;
+pop (@path), unshift (@path, q[-not (]), push (@path, ')') if @path;
+
+push @type, '-type', $_, qw(-or) foreach @find_types;
+pop (@type), unshift (@type, '('), push (@type, ')') if @type;
+
+exec qw(find .), @path, @type, @name;
--- src/bs.pl
+++ src/bs.pl
@@ -7,6 +7,11 @@ use warnings;
 
 use Cwd;
 
+# User-Serviceable Parts
+my @ignore_directories = qw(.git __pycache__ cover_db);
+my @find_types = qw(f);
+# End of User-Serviceable Parts
+
 my $me = (split m|/|, $0)[-1];
 
 @ARGV or die "Usage:\n\t$me <string pattern> [filname glob]...\n";
@@ -16,18 +21,18 @@ $gitroot = eval { `git rev-parse --show-toplevel` };
 chomp $gitroot;
 die "$me: Unsupported working directory or `git` not on \$PATH!\n"
     if $@ or not $gitroot;
+chdir $gitroot;
 
 my $search = shift;
+my (@name, @path, @type);
 
-my @name;
-push @name, (m|/| ? '-path' : '-name', "*$_*", '-o') foreach @ARGV;
+push @name, (m|/| ? '-ipath' : '-iname', "*$_*", '-o') foreach @ARGV;
 pop (@name), unshift (@name, '('), push (@name, ')') if @name;
 
-chdir $gitroot;
-exec qw(find . -not (
-                      -path */cover_db  -prune -or
-                      -path */.git      -prune -or
-                      -path */tmp       -prune -or
-                      -path */web/run   -prune
-                    )
-               -type f), @name, qw(-exec egrep -HnI --), $search, qw({} +);
+push @path, '-path', "*/$_", qw(-prune -or) foreach @ignore_directories;
+pop (@path), unshift (@path, q[-not (]), push (@path, ')') if @path;
+
+push @type, '-type', $_, qw(-or) foreach @find_types;
+pop (@type), unshift (@type, '('), push (@type, ')') if @type;
+
+exec qw(find .), @path, @type, @name, qw(-exec egrep -HnI --), $search, qw({} +);
