a44d04b9acf8173f7e69b2b8d539be4ba428842d
Ankit Pati <contact@ankitpati.in>
2020-04-12 16:44:14 +0530

    phf2pdf.pl: cosmetic fixes & remove unused imports

--- src/phf2pdf.pl
+++ src/phf2pdf.pl
@@ -3,21 +3,21 @@
 use strict;
 use warnings;
 use autodie;
-use feature qw(say);
 
 use File::Temp qw(tmpnam);
 use URI;
 use LWP::Simple qw(get);
 use PDF::Create;
-use DDP;
 
 use constant {
     # Download URL Format String
-    DL_URL_FMT_STR => 'https://online.pubhtml5.com'.'%s/files/large/%u.jpg',
+    DL_FMT => 'https://online.pubhtml5.com'.'%s/files/large/%u.jpg',
+
+    # X Scale Factor (between [0.00, 1.00], determined experimentally)
     XSCALE => 0.83,
 };
 
-my $me = (split m{/}, $0)[-1];
+my $me    = (split m{/}, $0)[-1];
 my $usage = "Usage:\n\t$me <https://pubhtml5.com/...> ...\n";
 
 @ARGV or die $usage;
@@ -27,23 +27,22 @@ my %paths;
 foreach my $link (@ARGV) {
     my $uri = URI->new ($link);
 
-    die $usage unless $uri->scheme eq 'https' and
-                      $uri->host eq 'pubhtml5.com' and
-                      $uri->path ne '';
+    die $usage unless $uri->scheme eq 'https'        and
+                      $uri->host   eq 'pubhtml5.com' and
+                      $uri->path   ne '';
 
     $paths{$link} = $uri->path;
 }
 
 foreach my $path (map { $paths{$_} } @ARGV) {
-    my $pdfname = $path;
-    $pdfname =~ s{\W}{}g;
-    $pdfname .= '.pdf';
+    my $pdfname  = $path;
+    $pdfname     =~ s{\W}{}g;
+    $pdfname    .= '.pdf';
 
-    my $pdf = PDF::Create->new (filename => $pdfname);
+    my $pdf  = PDF::Create->new (filename => $pdfname);
     my $root = $pdf->new_page (MediaBox => $pdf->get_page_size ('A2'));
 
-    my $page_number = 1;
-    while (my $jpg = get sprintf DL_URL_FMT_STR, $path, $page_number) {
+    for (my $pgnum = 1; my $jpg = get sprintf DL_FMT, $path, $pgnum; ++$pgnum) {
         my $img_tmp_filename = tmpnam . '-phf2pdf.jpg';
 
         open my $img_tmp_fh, '>', $img_tmp_filename;
@@ -53,17 +52,15 @@ foreach my $path (map { $paths{$_} } @ARGV) {
         $img_tmp_fh->close;
 
         my $image = $pdf->image ($img_tmp_filename);
-        my $page = $root->new_page;
+        my $page  = $root->new_page;
         $page->image (
-            xpos => 0,
-            ypos => 0,
+            xpos   => 0,
+            ypos   => 0,
             xscale => XSCALE,
-            image => $image,
+            image  => $image,
         );
 
         unlink $img_tmp_filename;
-
-        ++$page_number;
     }
 
     $pdf->close;
