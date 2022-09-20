99f22021d06782e950fdd16887d3a2f0974bb783
Ankit Pati <contact@ankitpati.in>
2022-09-20 11:54:07 +0530

    add Java-based scripts for ZIP DEFLATE handling

---
+++ src/in/ankitpati/zip/InputDeflater.java
@@ -0,0 +1,19 @@
+package in.ankitpati.zip;
+
+import java.io.IOException;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterInputStream;
+
+public class InputDeflater {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 0) {
+            System.err.println("Usage:\n\tjava InputDeflater < input.txt > output.dfl");
+            System.exit(1);
+        }
+
+        try (DeflaterInputStream inputStream =
+                new DeflaterInputStream(System.in, new Deflater(Deflater.DEFLATED, true))) {
+            inputStream.transferTo(System.out);
+        }
+    }
+}
---
+++ src/in/ankitpati/zip/InputInflater.java
@@ -0,0 +1,19 @@
+package in.ankitpati.zip;
+
+import java.io.IOException;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+public class InputInflater {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 0) {
+            System.err.println("Usage:\n\tjava InputInflater < input.dfl > output.txt");
+            System.exit(1);
+        }
+
+        try (InflaterInputStream inputStream =
+                new InflaterInputStream(System.in, new Inflater(true))) {
+            inputStream.transferTo(System.out);
+        }
+    }
+}
